b3eb9572a7cafbe8bbc8f6fc371556aecf536a77
Ocean <notbad1520@gmail.com>
2017-12-07 21:09:36 +0300

    Add parametrized test with enum source

--- src/test/java/demo/ParameterizedTestExample.kt
+++ src/test/java/demo/ParameterizedTestExample.kt
@@ -1,9 +1,12 @@
 package demo
 
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import org.junit.jupiter.params.provider.ValueSource
 import java.time.LocalDate
+import java.util.concurrent.TimeUnit
 
 class ParameterizedTestExample {
 
@@ -11,5 +14,17 @@ class ParameterizedTestExample {
     @ValueSource(strings = ["2002-01-23", "1956-03-14", "1503-07-19"])
     fun `Check date in past`(date: LocalDate) {
         assertTrue(date.isBefore(LocalDate.now()))
+    }
+
+    @ParameterizedTest
+    @EnumSource(TimeUnit::class)
+    fun `Test enum`(timeUnit: TimeUnit) {
+        assertNotNull(timeUnit)
+    }
+
+    @ParameterizedTest
+    @EnumSource(TimeUnit::class, mode = EnumSource.Mode.EXCLUDE, names = ["SECONDS", "MINUTES"])
+    fun `Test enum without days and milliseconds`(timeUnit: TimeUnit) {
+        assertNotNull(timeUnit)
     }
 }

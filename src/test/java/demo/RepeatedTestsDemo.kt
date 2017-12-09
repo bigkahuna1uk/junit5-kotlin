8a66945df9a651bc433e09f6deef2dd4622bcd5a
Ocean <notbad1520@gmail.com>
2017-12-09 11:12:04 +0300

    Add repeated test example

--- src/test/java/demo/RepeatedTestsDemo.kt
+++ src/test/java/demo/RepeatedTestsDemo.kt
@@ -1,11 +1,22 @@
 package demo
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.RepetitionInfo
+import org.junit.jupiter.api.TestInfo
 
 class RepeatedTestsDemo {
 
     @RepeatedTest(10, name = "{displayName} {currentRepetition} из {totalRepetitions}")
     fun `Повторяемый тест`() {
 
+    }
+
+    @RepeatedTest(5)
+    fun `Repeated test with repetition and test info`(repetitionInfo: RepetitionInfo, testInfo: TestInfo) {
+        assertEquals(5, repetitionInfo.totalRepetitions)
+        val testDisplayNameRegex = """repetition \d of 5""".toRegex()
+        assertTrue(testInfo.displayName.matches(testDisplayNameRegex))
     }
 }
